efe416e43644d3712b6fde9decd7d428c169eef8
Shaun McBrearty <Shaun.McBrearty@atu.ie>
2023-11-30 21:43:31 +0000

    Add project files.

---
+++ C-Sharp Secure Coding - In-Memory Encryption/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+using C_Sharp_Secure_Coding___In_Memory_Encryption;
+
+//Plaintext
+
+String valueToEncrypt = "Hello World";//Value To Be Encrypted
+
+//Encrypt
+
+byte[] plaintext = ASCIIEncoding.ASCII.GetBytes(valueToEncrypt);//Binary Encoded Version Of Value To Be Encrypted
+byte[] ciphertext = Windows_DPAPI.Protect(plaintext, true, System.Security.Cryptography.DataProtectionScope.LocalMachine);//Encrypt Data
+
+Console.WriteLine("Plaintext Value (Before Encryption): " + valueToEncrypt);
+Console.WriteLine("");
+Console.WriteLine("Ciphertext Value (After Encryption): " + System.Convert.ToBase64String(ciphertext));
+Console.WriteLine("");
+
+//Remove Plaintext Values From Memory
+
+valueToEncrypt = null;
+plaintext = null;
+GC.Collect();//Invoke Garbage Collector
+
+//Decrypt
+
+plaintext = Windows_DPAPI.Unprotect(ciphertext, true, System.Security.Cryptography.DataProtectionScope.LocalMachine);//Decrypt Data
+String decryptedValue = ASCIIEncoding.ASCII.GetString(plaintext);//Recreate Original Object (String) From Binary Encoded Version
+Console.WriteLine("Plaintext Value (After Decryption): " + decryptedValue);
---
+++ C-Sharp Secure Coding - In-Memory Encryption/Windows_DPAPI.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Secure_Coding___In_Memory_Encryption
+{
+    internal class Windows_DPAPI
+    {
+
+        static readonly int No_Of_IV_Bytes = 16;//16 Bytes => 128 Bits
+
+        public static byte[] Protect(byte[] plaintext, bool includeAdditionalEntropy, DataProtectionScope dpsValue)
+        {
+            
+            if(includeAdditionalEntropy == false)
+            {
+                return ProtectedData.Protect(plaintext, null, dpsValue);//null => No IV Value Supplied, Resulting In Deterministically Encrypted Value.
+            }
+                
+            else
+            {
+                byte[] iv = RandomNumberGenerator.GetBytes(Windows_DPAPI.No_Of_IV_Bytes);//Generate IV Value
+                byte[] ciphertext = ProtectedData.Protect(plaintext, iv, dpsValue);//Encrypt Data Using Windows DPAPI
+                    
+                byte[] result = new byte[iv.Length + ciphertext.Length];//Byte Array Where IV And Ciphertext Values Will Be Stored Together
+                Array.Copy(iv, result, iv.Length);//Store IV Value At Beginning Of The 'result' Array.
+                Array.Copy(ciphertext, 0, result, iv.Length, ciphertext.Length);//Store Ciphertext Value Directly After IV Value In The 'result' Array.
+
+                return result;
+
+            }
+            
+        }
+
+        public static byte[] Unprotect(byte[] ciphertextInclIV, bool includeAdditionalEntropy, DataProtectionScope dpsValue)
+        {
+            if (includeAdditionalEntropy == false)
+            {
+                return ProtectedData.Unprotect(ciphertextInclIV, null, dpsValue);
+            }
+
+            else
+            {
+                byte[] iv = new byte[Windows_DPAPI.No_Of_IV_Bytes];//Byte Array Where IV Value Will Be Extracted To.
+                byte[] ciphertext = new byte[ciphertextInclIV.Length - Windows_DPAPI.No_Of_IV_Bytes];//Byte Array Where Ciphertext Value Will Be Extracted To.
+
+                Array.Copy(ciphertextInclIV, iv, iv.Length);//Extract IV Value And Assign To 'iv' Byte Array.
+                Array.Copy(ciphertextInclIV, iv.Length, ciphertext, 0, ciphertext.Length);//Extract Ciphertext Value And Assign To 'ciphertext' Byte Array.
+                return ProtectedData.Unprotect(ciphertext, iv, dpsValue);//Decrypt Data Using Windows DPAPI.
+
+            }
+        }
+
+    }
+}
